625a4cada127276dfcc252c8e2c874284ac9e654
Matt Smith <mattsmith1652@gmail.com>
2025-10-25 18:00:25 -0400

    params type fix in chart layout component

--- src/app/simulation/[sessionId]/chart/layout.tsx
+++ src/app/simulation/[sessionId]/chart/layout.tsx
@@ -8,7 +8,6 @@ import { useDispatch } from 'react-redux';
 import { initializeTime, updateSimulationTime } from './components/timeSlice';
 import type { AppDispatch } from '../../../store/store.js'
 import Header from "@/app/simulation/[sessionId]/chart/components/header"
-import { useParams } from "next/navigation";
 
 type ChartLayoutProps = {
   children: React.ReactNode;
@@ -17,7 +16,6 @@ type ChartLayoutProps = {
 // params is dynamic URL portion
 const ChartLayout = ({ children }: ChartLayoutProps) => {
   const dispatch = useDispatch<AppDispatch>();
-  const params = useParams()
   // Main component of the entire chart, time is kept here
   // Timer sets the sessionStartTime once and updates the simulation time for components that need it (FlexSheet, Overview Cards, Labs)
   // probably better ways of doing this
@@ -31,9 +29,6 @@ const ChartLayout = ({ children }: ChartLayoutProps) => {
     return () => clearInterval(interval);
   }, []);
 
-
-  const { sessionId } =  params
-  
   return (
     <div className="bg-lime-600 h-screen w-full overflow-hidden flex flex-col [--header-height:calc(--spacing(16))]">
       <Toaster position="top-right" />
